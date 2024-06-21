d8d9a22b8a66a35898b9251051648d7a423295f2
Rohila <asrohila03@gmail.com>
2024-06-21 20:05:56 +0530

    update header and make profile pg private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 // import React from 'react'
-import{ BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -13,11 +14,13 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={ <About/>} />
-        <Route path='/sign-in' element={ <SignIn/>} />
-        <Route path='/sign-up' element={ <SignUp/>} />
-        <Route path='/profile' element={ <Profile/>} />
-      </Routes>    
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />} >
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,37 +1,46 @@
 import { FaSearch } from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
-  return (
-      <header className='bg-sky-200/80 shadow-md'>
-          
-          <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-              <Link to='/'>
-          <h1 className='font-bold text-base sm:text-2xl flex flex-wrap'>
-          <span className='text-sky-400'>Real</span>
-          <span>Estate</span>
-              </h1>
-              </Link>
 
-              <form className='bg-sky-50 p-3 rounded-lg flex items-center'>
-                  <input type="text"
-                      placeholder='Search'
-                      className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                  <FaSearch />
-              </form>
+    const { currentUser } = useSelector(state => state.user)
 
-              <ul className='flex gap-6 font-semibold'>
-                  <Link to='/'>
-                  <li className='hidden sm:inline hover:text-sky-600 active:text-sky-400'>Home</li>
-                  </Link>
-                  <Link to='/about'>
-                  <li className='hidden sm:inline hover:text-sky-600 active:text-sky-400'>About</li>
-                  </Link>    
-                  <Link to='/sign-up'>
-                  <li className='hover:text-sky-600 active:text-sky-400'>Sign In</li>
-                  </Link>
-              </ul>
-          </div>
-      </header>
-  )
+    return (
+        <header className='bg-sky-200/80 shadow-md'>
+
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-base sm:text-2xl flex flex-wrap'>
+                        <span className='text-sky-400'>Real</span>
+                        <span>Estate</span>
+                    </h1>
+                </Link>
+
+                <form className='bg-sky-50 p-3 rounded-lg flex items-center'>
+                    <input type="text"
+                        placeholder='Search'
+                        className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                    <FaSearch />
+                </form>
+
+                <ul className='flex gap-6 font-semibold'>
+                    <Link to='/'>
+                        <li className='hidden sm:inline hover:text-sky-600 active:text-sky-400'>Home</li>
+                    </Link>
+                    <Link to='/about'>
+                        <li className='hidden sm:inline hover:text-sky-600 active:text-sky-400'>About</li>
+                    </Link>
+                    <Link to='/profile'>
+                        {currentUser
+                            ? (<img
+                                className='rounded-full h-7 w-7 object-cover'
+                                src={currentUser.data.user.avatar} alt="profile" />)
+                            : (<li className='hover:text-sky-600 active:text-sky-400'>Sign In</li>)
+                        }
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    )
 }
