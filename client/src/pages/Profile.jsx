57c288fe810754cfa2f577fe9f1ee9ab96c5f320
Rohila <asrohila03@gmail.com>
2024-06-22 09:53:57 +0530

    complete profile pg UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+
+  const { currentUser } = useSelector(state => state.user)
+
   return (
-    <div>Profile</div>
+    <div
+    className='p-3 max-w-lg mx-auto'>
+      <h1
+      className="text-3xl font-semibold text-center my-7">
+        Profile
+      </h1>
+      
+      <form
+      className='flex flex-col gap-4'>
+        <img
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+          src={currentUser.data.user.avatar} alt="profile" />
+        <input
+          type="text"
+          placeholder='username'
+          id='username'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type="text"
+          placeholder='email'
+          id='email'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type="text"
+          placeholder='password'
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+
+        <button
+        className='bg-sky-600 text-white p-3 rounded-lg hover:opacity-90 active:bg-sky-500  disabled:opacity-80'>
+          UPDATE
+        </button>
+      </form>
+
+      <div
+      className='flex justify-between mt-5'>
+        <span
+        className='text-red-700 cursor-pointer hover:opacity-85'>
+          Delete Account
+        </span>
+        <span
+        className='text-red-700 cursor-pointer hover:opacity-85'>
+          Sign Out
+        </span>
+      </div>
+    </div>
   )
 }
