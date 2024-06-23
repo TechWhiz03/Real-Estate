e5d2fe2f4dde6144635b3e07885ddea56eac0ac1
Rohila <asrohila03@gmail.com>
2024-06-23 16:04:42 +0530

    complete create listing pg UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
   signOutUserSuccess,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
 
@@ -179,6 +180,12 @@ export default function Profile() {
           className='bg-sky-600 text-white p-3 rounded-lg hover:opacity-90 active:bg-sky-500  disabled:opacity-80'>
           {loading ? 'Loading...' : 'UPDATE'}
         </button>
+
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'} >
+          Create Listing
+        </Link>
       </form>
 
       <div
