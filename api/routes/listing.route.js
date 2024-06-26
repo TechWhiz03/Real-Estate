51b97bd88acc859d589afca3904a752999f322e2
Rohila <asrohila03@gmail.com>
2024-06-26 10:17:56 +0530

    complete delete user listing func

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {
     createListing,
-    // deleteListing,
+    deleteListing,
     // updateListing,
     // getListing,
     // getListings
@@ -11,7 +11,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
-// router.delete('/delete/:id', verifyToken, deleteListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 // router.post('/update/:id', verifyToken, updateListing);
 // router.get('/get/:id', getListing);
 // router.get('/get', getListings);
