eb326fe79a5cf1c4ef9f1e0c79854701d32e8214
Rohila <asrohila03@gmail.com>
2024-06-28 12:27:48 +0530

    complete header search form func

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
 import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react';
 
 export default function Header() {
 
     const { currentUser } = useSelector(state => state.user)
+    const [ searchTerm, setSearchTerm ] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [ location.search ]);
 
     return (
         <header className='bg-sky-200/80 shadow-md'>
@@ -17,11 +36,18 @@ export default function Header() {
                     </h1>
                 </Link>
 
-                <form className='bg-sky-50 p-3 rounded-lg flex items-center'>
+                <form
+                    onSubmit={handleSubmit}
+                    className='bg-sky-50 p-3 rounded-lg flex items-center'>
                     <input type="text"
                         placeholder='Search'
-                        className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                    <FaSearch />
+                        className='bg-transparent focus:outline-none w-24 sm:w-64'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <button>
+                        <FaSearch className='text-sky-600' />
+                    </button>
                 </form>
 
                 <ul className='flex gap-6 font-semibold'>
