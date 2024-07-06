17eb9e0d5e427f8b3878c849f93eb221fe2d25f5
Rohila <asrohila03@gmail.com>
2024-07-06 10:07:49 +0530

    add pre installation for render deploy

--- api/index.js
+++ api/index.js
@@ -4,8 +4,11 @@ import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
+
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +28,12 @@ app.listen(process.env.PORT || 8000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
